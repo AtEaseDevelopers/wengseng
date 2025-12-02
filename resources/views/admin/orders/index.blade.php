bd4186505449a16b81b4100299a45d3f3e8a8abe
Msiang <mou20181234@gmail.com>
2025-12-02 21:33:02 +0800

    Update index.blade.php

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -263,7 +263,7 @@
                                         </td>
                                         <td class="text-center">{!! __('order.status.' . $order->status) !!}</td>
                                         <td class="text-center">
-                                            {!! isset($order->sql_sync_status, $order->sql_sync_respond)
+                                            {!! isset($order->sql_sync_status)
                                                 ? \App\Helper::sql_sync_status_badge($order->sql_sync_status, $order->sql_sync_respond)
                                                 : '<span class="text-muted">—</span>' !!}
                                         </td>
