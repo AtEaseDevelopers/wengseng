0033cebe3fa0040306ec95df50a80dc983603c72
yx13 <exian97@gmail.com>
2025-12-08 17:09:22 +0800

    update code

--- app/Exports/BarangLebihSheetExport.php
+++ app/Exports/BarangLebihSheetExport.php
@@ -111,7 +111,7 @@ class BarangLebihSheetExport implements FromCollection, WithHeadings, WithTitle,
             $productTotal = 0;
             $hasData = false;
             
-            if ($product != null && !in_array($product->category_name, $new_category)) {
+            if ($product != null && isset($product->category_name) && !in_array($product->category_name, $new_category)) {
                 $rows[] = ['Category' => $product->category_name];
                 $new_category[] = $product->category_name;
             }
